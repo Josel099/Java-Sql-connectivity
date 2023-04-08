54a2e814162312691b04d7a7360d28701b3cd46e
Josel Xavier <joseltech2002@gmail.com>
2023-04-08 21:54:07 +0530

    created statement

--- Sql1/src/mysqldemos/mysqlDMLcommands.java
+++ Sql1/src/mysqldemos/mysqlDMLcommands.java
@@ -1,5 +1,41 @@
 package mysqldemos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//1. Create a connection
+//2. Create statement/Query
+//3. Execute statment/Query
+//4. Store the results in result set
+//5.  close connection
+
 public class mysqlDMLcommands {
 
+	public static void main(String[] args) throws SQLException {
+		
+		
+		//1. Create a connection
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","joselxavier");
+		
+		
+		
+		//2. Create statement/Query
+		
+		Statement stmt = con.createStatement();
+
+		
+		String s="INSERT INTO STUDENT VALUES(4,'raghu')";
+		
+		
+		
+		
+		
+		
+		
+		
+	}  
+	
 }
